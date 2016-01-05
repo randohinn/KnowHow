6f438c1c1a197624db0de7bf0527f75bac677ed3
Rando Hinn <randohinn@gmail.com>
2016-01-05 16:25:06 +0200

    Over-simplify konf

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-import os
-import sys
-
-from recommonmark.parser import CommonMarkParser
-
-sys.path.insert(0, os.path.abspath('..'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.sqlite")
-
-from django.conf import settings
-
-import django
-django.setup()
-
-
-sys.path.append(os.path.abspath('_ext'))
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx_http_domain',
-    'djangodocs',
-]
-templates_path = ['_templates']
-
-source_suffix = ['.rst', '.md']
-source_parsers = {
-    '.md': CommonMarkParser,
-}
 
 master_doc = 'index'
 project = u'KnowHow'
 copyright = u'2015-2016 Rando Hinn'
-version = '1.0'
-release = '1.0'
-exclude_patterns = ['_build']
-default_role = 'obj'
-pygments_style = 'sphinx'
