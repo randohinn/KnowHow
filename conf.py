1e33c013b969bdbcedb84ded4d7179339bccfda1
Rando Hinn <randohinn@gmail.com>
2016-01-05 16:07:26 +0200

    Konf. try 3

--- conf.py
+++ conf.py
@@ -1,5 +1,6 @@
-master_doc = 'index'
-project = u'KnowHow'
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
 copyright = u'2015-2016 Rando Hinn'
-version = '1.0'
-release = '1.0'
